3558d26b2579d0d2c0599e6889bb62c7e2ce699f
Michele Prova <michele.prova99@gmail.com>
2020-07-20 08:24:46 +0200

    da sistemare ancora qualcosina

--- js/script.js
+++ js/script.js
@@ -7,8 +7,55 @@
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla",
 // se clicchiamo su annulla dobbiamo ripulire il form.
 
-// Quando cliccherò il bottone "Genera" apparirà il div biglietto
 
-// document.getElementById("btn1").addEventListener("click", function() {
-//
-// document.getElementById("biglietto").className = " animation";
+var genera = document.getElementById("btn1");
+
+
+genera.addEventListener("click",
+function () {
+  document.getElementById("biglietto").className = "show";
+
+
+  var nome = document.getElementById("nome").value;
+  document.getElementById("nome-passeggero").innerHTML = nome;
+
+
+  var carrozza = generaRandom(1,10);
+  var codiceCp = generaRandom(90000,100000);
+  document.getElementById("carrozza").innerHTML = carrozza;
+  document.getElementById("codice-cp").innerHTML = codiceCp;
+
+
+  var km = document.getElementById("km").value;
+  var prezzo = km * 0.21;
+  var messaggio = "Tariffa Standart";
+
+
+  var eta = document.getElementById("fascia-eta").value;
+
+  if (eta == "Minorenne") {
+    prezzo -= prezzo * 0.2;
+    messaggio = "Tariffa Minorenne";
+  } else if (eta == "Over") {
+    prezzo -= prezzo * 0.4;
+    messaggio = "Tariffa Silver";
+  }
+
+  document.getElementById("offerta2").innerHTML = messaggio;
+  document.getElementById("costo2").innerHTML = prezzo.toFixed(2);
+
+
+}
+);
+
+var annulla = document.getElementById("btn2");
+
+annulla.addEventListener("click",
+function () {
+  document.getElementById("biglietto").className = "hidden";
+}
+);
+
+function generaRandom(min,max) {
+  return Math.floor(Math.random()*(max - min) + min);
+}
